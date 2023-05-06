173b8c2ba19cd6b02aa685ca4456b24547521b71
Ankit <ankitkumar19041@.com>
2023-05-06 14:33:02 +0530

    feat: added all the carousels for the home page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,5 @@
-import Navbar from "@/components/navbar";
 import { ILayoutProps } from "@/types";
 
 export default function HomeLayout({ children }: ILayoutProps) {
-  return (
-    <>
-      <Navbar />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
