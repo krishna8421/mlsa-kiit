72cb5488f469b6d6cc8e06da7714ce58d5ef5b90
Krishna Kumar <dev@krishnaaa.com>
2023-05-17 11:30:10 +0530

    ui: styles in error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
@@ -8,9 +9,9 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={reset}>Try again</button>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-white">
+      <h2 className="text-lg font-medium">Something went wrong!</h2>
+      <Button onClick={reset}>Try again</Button>
     </div>
   );
 }
