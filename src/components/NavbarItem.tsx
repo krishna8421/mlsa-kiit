6abd9acf206ffb89b3b074feef1d077d6e5f058d
Krishna Kumar <dev@krishnaaa.com>
2023-05-17 13:06:13 +0530

    refactor: same component for all links in navbar

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -3,12 +3,23 @@ import Link from "next/link";
 
 interface NavbarItemProps extends ILayoutProps {
   href: string;
+  type?: "button" | "link";
 }
 
-const NavbarItem = ({ children, href }: NavbarItemProps) => {
+const NavbarItem = ({ children, href, type = "link" }: NavbarItemProps) => {
+  if (type === "button") {
+    return (
+      <Link href={href}>
+        <button className="2xl:text-md rounded-full bg-gradient-blue px-7 py-2 text-sm text-white">
+          {children}
+        </button>
+      </Link>
+    );
+  }
+
   return (
     <Link href={href}>
-      <div className="cursor-pointer text-sm transition hover:text-gray-500 2xl:text-md">
+      <div className="2xl:text-md cursor-pointer text-sm transition hover:text-gray-500">
         {children}
       </div>
     </Link>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,18 +11,16 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
           <Image src="/mlsa-logo.png" alt="MLSA Logo" width={40} height={40} loading="eager" />
         </div>
-        <div className="flex items-center gap-10 2xl:gap-[2.3vw]">
-          <div className="hidden items-center gap-10 font-normal lg:flex">
-            <NavbarItem href="/events">Events</NavbarItem>
-            <NavbarItem href="/projects">Projects</NavbarItem>
-            <NavbarItem href="/blogs">Blog</NavbarItem>
-            <NavbarItem href="/gallery">Gallery</NavbarItem>
-            <NavbarItem href="/teams">Teams</NavbarItem>
-            <NavbarItem href={YOUTUBE_URL}>Youtube</NavbarItem>
-          </div>
-          <button className="2xl:text-md rounded-full bg-gradient-blue px-7 py-2 text-sm text-white">
+        <div className="hidden items-center gap-8 md:flex">
+          <NavbarItem href="/events">Events</NavbarItem>
+          <NavbarItem href="/projects">Projects</NavbarItem>
+          <NavbarItem href="/blogs">Blog</NavbarItem>
+          <NavbarItem href="/gallery">Gallery</NavbarItem>
+          <NavbarItem href="/teams">Teams</NavbarItem>
+          <NavbarItem href={YOUTUBE_URL}>Youtube</NavbarItem>
+          <NavbarItem href="/contact" type="button">
             Contact
-          </button>
+          </NavbarItem>
         </div>
       </div>
     </nav>
