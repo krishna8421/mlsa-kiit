cdbd31185cbf732d9a1578e86f66a18674d07cc7
manishmh <manishmh982@gmail.com>
2023-05-25 12:29:27 +0530

    ui(padding and footer): changed padding of sponsers and contact us for mobile view
    added text center for mobile view and text start for md view

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const ContactUs = () => {
   return (
-    <div className=" mt-32 flex w-full flex-col md:mt-36 md:h-[339px] md:flex-row ">
+    <div className="mt-32 flex w-full flex-col md:mt-36 md:h-[339px] md:flex-row ">
       <Image
         width={1920}
         height={1080}
@@ -12,7 +12,7 @@ const ContactUs = () => {
       />
 
       <div className=" bg-[#f4f4f4] md:w-[63%]">
-        <div className="pl-[90px] pt-[70px] text-2xl font-bold">
+        <div className="pl-8 md:pl-[90px] pt-[70px] text-2xl font-bold">
           <h1 className="text-[#286FD9]">Join</h1>
           <h1>
             Micrsoft Learn <span className=" text-[#286FD9]">Student</span>
@@ -21,7 +21,7 @@ const ContactUs = () => {
           <h1 className="font-normal">KIIT Chapter</h1>
         </div>
         <Link href="#" className="">
-          <button className="my-[24px] mb-16 ml-[82px] flex h-[44px] w-[151px] items-center justify-center whitespace-nowrap rounded-[100px] bg-gradient-blue  px-7 py-[15px] text-xl  text-white ">
+          <button className="my-[24px] mb-16 ml-8 md:ml-[82px] flex h-[44px] w-[151px] items-center justify-center whitespace-nowrap rounded-[100px] bg-gradient-blue  px-7 py-[15px] text-2xl  text-white ">
             Contact Us
           </button>
         </Link>
@@ -31,3 +31,4 @@ const ContactUs = () => {
 };
 
 export default ContactUs;
+
