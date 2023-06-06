473d7f1f8eff084a970df004a0e2bff303bc07a1
Harshit <2105891@kiit.ac.in>
2023-06-06 23:22:45 +0530

    homepage lead caraousel thk hogya

--- src/components/carousels/OurLeads.tsx
+++ src/components/carousels/OurLeads.tsx
@@ -1,37 +1,61 @@
 "use client";
 
 import { OUR_LEADS } from "@/constants";
-import { Navigation } from "swiper";
+import { Autoplay, Navigation } from "swiper";
+import "swiper/css/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
 import LeadsCard from "./LeadsCard";
 
-import "swiper/swiper-bundle.min.css";
-// import "swiper/css/bundle";
-
 const OurLeads = () => {
-  // const [LeadsData, setLeadsData] = useState([]);
-  // useEffect(() => {
-  //   const fetachLeads = async () => {
-  //     const response = await fetch("https://locahost:3000/api/alumuni");
-  //     const data = await response.json();
-  //     setLeadsData(data);
-  //   };
-  //   // fetachLeads();
-  // }, []);
   return (
-    <div className=" text-center">
+    <div className=" h-[40vh] w-[100vw] text-center  ">
       <h1 className="mb-9 text-lg font-semibold md:text-3xl">
         Our <span className="text-[#3B61CF]">Leads</span>
       </h1>
       <Swiper
-        className="px-3 md:ml-20"
+        className="ml-[10%] flex h-[100%] w-[90%] items-center justify-center  px-3 md:ml-20  "
         spaceBetween={14}
-        slidesPerView="auto"
+        slidesPerView={2.6}
         cssMode={true}
         loop={false}
         mousewheel={true}
         navigation={true}
-        modules={[Navigation]}
+        // centeredSlides={true}
+        // Autoplay={true}
+        modules={[Navigation, Autoplay]}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 100,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 1.8,
+            spaceBetween: 80,
+          },
+          800: {
+            slidesPerView: 2.1,
+            spaceBetween: 90,
+          },
+          900: {
+            slidesPerView: 2.3,
+            spaceBetween: 90,
+          },
+          1000: {
+            slidesPerView: 3.1,
+            spaceBetween: 250,
+          },
+          1200: {
+            slidesPerView: 3.34,
+            spaceBetween: 250,
+          },
+        }}
       >
         {OUR_LEADS.map((Lead, index) => {
           return (
