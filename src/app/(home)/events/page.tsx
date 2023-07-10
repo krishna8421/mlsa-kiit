b4047419b09e731dd433cd28320fa904e84c0b23
A01Akshat <akshat29december@gmail.com>
2023-07-10 19:23:58 +0530

    events card

--- src/app/(home)/events/page.tsx
+++ src/app/(home)/events/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import EventCard from '@/components/EventCard'
 import EventHero from '@/components/EventHero'
 
 const Events = () => {
   return (
     <div>
-      <EventHero/>
+      <EventHero />
+      <EventCard />
     </div>
   )
 }
