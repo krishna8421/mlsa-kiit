05efcb47ee4c5bfb69dcf3adde8a12c9bebd27da
Ankit <ankitkumar19041@gmail.com>
2023-07-10 17:35:17 +0530

    minute animation changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 // import OurAlumuni from "@/components/Alumni";
 import Alumni1 from "@/components/Alm";
 import JoinUs from "@/components/JoinUs";
+import Leads from "@/components/LeadsAnimated";
 import OurLeads from "@/components/OurLeads";
 import Hero from "@/components/Hero";
 import ScrollAnimationLead from "@/components/ScrollAnimationLeads";
@@ -21,15 +22,16 @@ export default function Home() {
       <About />
 
       <Domain />
-      {/* <OurLeads/> */}
+
+      <Leads />
       <JoinUs />
-      {/* <ScrollAnimationLead/> */}
+      {/* <ScrollAnimationLead /> */}
       {/* <OurAlumuni/> */}
       <Alumni1 />
       {/* <ScrollAnimationCode/> */}
 
-      <Domain />
-      <ScrollAnimationLead />
+
+
       <ScrollAnimationCode />
 
     </div>
