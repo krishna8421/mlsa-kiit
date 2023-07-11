8ceb85f44f1f5dfb3b99138d475ce5b011641791
manishmh <manishmh982@gmail.com>
2023-07-11 10:39:32 +0530

    fixed about us and domain alignment

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -11,7 +11,7 @@ const AboutCard = ({ icon, label, Classname, content }: aboutCardProps) => {
   return (
     <>
       <div
-        className={`md:max-w-[400px] flex-shrink-0 lg:w-[30vw] lg:max-w-[350px] 2xl:max-w-[423px] xl:w-[23vw] group rounded-xl bg-[#252525]/50 px-4 py-8  duration-400 transition shadow-lg hover:shadow-aboutContainerShadow ${Classname}`}
+        className={`md:max-w-[330px] md:w-[35vw] flex-shrink-0 lg:w-[30vw] lg:max-w-[350px] 3xl:max-w-[423px] xl:w-[20vw] lg:min-w-[280px] w-full group rounded-xl bg-[#252525]/50 px-4 md:py-6 lg:py-4 duration-400 transition shadow-lg hover:shadow-aboutContainerShadow ${Classname}`}
       >
         {/* <Image src="/Rectangle.png" alt="" width={700} height={700} className="absolute top-0"></Image> */}
         <div className="flex items-center justify-between">
@@ -21,7 +21,7 @@ const AboutCard = ({ icon, label, Classname, content }: aboutCardProps) => {
           <div className="mr-2 h-[3px] md:h-[5px] w-full bg-gradient-to-l from-[#4b8ff4] to-transparent"></div>
           <div className="text-xl md:text-2xl text-white 3xl:text-3xl">{label}</div>
         </div>
-        <p className="pt-8 text-[#7d8590] duration-400 transition text-base xl:text-md group-hover:text-white 2xl:text-lg 3xl:text-xl">
+        <p className="pt-8 text-[#7d8590] duration-400 transition text-base xl:text-base group-hover:text-white 3xl:text-xl">
           {content}
         </p>
       </div>
--- src/components/Domain.tsx
+++ src/components/Domain.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { OUR_DOMAINS } from "@/constants";
-import Image from "next/image";
-import DomainCard from "./DomainCard";
 import { motion, useAnimation } from "framer-motion";
+import Image from "next/image";
 import React, { useEffect } from "react";
+import DomainCard from "./DomainCard";
 
 
 const Domain: React.FC = () => {
@@ -66,28 +66,46 @@ const Domain: React.FC = () => {
 
   return (
     <>
+      {/* <div className="mb-[100px] flex flex-col items-center justify-center">
+      <h1 className="mb-12 text-center text-xl font-semibold md:mb-0 md:text-3xl lg:leading-[40px] 2xl:text-[2.1vw] 2xl:leading-[6vh]">
+        Our <span className="text-[#3B61CF]">Domain</span>
+      </h1>
+      <div className="flex w-[325px] flex-wrap gap-7 md:w-[672px] lg:w-[1020px]">
+        {OUR_DOMAINS.map((domain, index) => {
+          return (
+            <DomainCard
+              key={index}
+              logo={<Image src={domain.imgSrc} alt="support" width={500} height={500}></Image>}
+              heading={domain.name}
+              about={domain.about}
+            ></DomainCard>
+          );
+        })}
+      </div>
+    </div> */}
 
+      <div className="min-h-[100px] pl-8 overflow-y-hidden pt-10 md:pl-[50px] lg:pl-[80px] xl:pl-[90px] 2xl:pl-[130px] mt-[100px] h-auto md:pb[222px] pb-[128px] relative z-0 flex flex-row max-w-[1920px] justify-center items-center mx-auto">
+        {/* <div className="h-[439px] w-[439px] absolute z-10 rounded full -top-[300px] -left-[578px]">
+<Image src="/domainLogos/Rectangle.png" alt="gradient" width={739} height={739} className="object-fit h-[739px] w-[739px] rounded-full bg-no-repeat object-cover blur-[155.5px]"></Image>
+</div> */}
 
-      <div className="min-h-[100px] h-auto md:pb-[222px] pb-[128px] relative z-0 flex flex-row max-w-[1920px] justify-center items-center mx-auto">
-
-
-        <div className="mx-auto flex -translate-y-[60vh] md:-translate-y-[30vh] sm:ml-[80px]">
+        <div className="mx-auto flex w-full h-full">
           <motion.div
-            className="flex justify-center"
-            initial={{ opacity: 0, y: 1000 }} //before scrolling position of slider
-            animate={controls} //refer to line 20
-            transition={{ duration: 1 }} //slider duration for popping up. if you are changinng duration then make sure to change delay of glow adder so thatt  the gow starts after animation is complete
-            onAnimationComplete={handleSliderAnimationComplete}
-          >
-            <div className="flex flex-col items-center">
-              <div
-                className="flex h-[41px] w-[41px] items-center justify-center rounded-full bg-[#A93BCF] bg-gradient-to-b from-[#473BCF]  to-[#fff]/30 shadow-dotShadowBlue duration-700 transition 
+          className="flex justify-center"
+          initial={{ opacity: 0, y: 1000 }} //before scrolling position of slider
+          animate={controls} //refer to line 20
+          transition={{ duration: 1 }} //slider duration for popping up. if you are changinng duration then make sure to change delay of glow adder so thatt  the gow starts after animation is complete
+          onAnimationComplete={handleSliderAnimationComplete}
+        >
+          <div className="flex flex-col items-center ">
+            <div
+              className="flex h-[41px] w-[41px] items-center justify-center rounded-full bg-[#A93BCF] bg-gradient-to-b from-[#473BCF]  to-[#fff]/30 shadow-dotShadowBlue duration-700 transition 
               "
-              >
-                <div className="h-[12px] w-[12px] self-center rounded-full bg-white"></div>
-              </div>
-              <div className="h-full w-[5px]  bg-gradient-to-b from-[#533BD0] to-transparent duration-700 transition lg:h-[80vh]"></div>
+            >
+              <div className="h-[12px] w-[12px] self-center rounded-full bg-white"></div>
             </div>
+            <div className="h-full w-[5px]  bg-gradient-to-b from-[#533BD0] to-transparent duration-700 transition lg:h-[80vh]"></div>
+          </div>
           </motion.div>
           {/* <div className="h-full ml-[7px] sm:ml-[31px] md:ml-[53px] 2xl:[120px] flex flex-col items-center ">
  <div className="md:w-[84px] md:h-[84px] w-[48] h-[48] border border-cyan-300">
@@ -98,34 +116,34 @@ const Domain: React.FC = () => {
   </div>
 </div> */}
           <div className="">
-            <motion.div
-              initial={{ opacity: 0 }} //initial position of text with opacity 0
-              animate={textControls} //text conntrol animation triggered.
-              transition={{ duration: 2 }}
-            >
-              <h1 className="sm:mb-[7px] ml-[11px] sm:ml-[15px] lg:ml-[30px] 2xl:ml-[53px] text-[12px] sm:text-[18px] font-semibold md:mb-[20px] md:text-3xl items-start text-[#FFFFFF] 2xl:text-[2.1vw] ">
-                Our <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#A03BCF] to-[#6F5BFF]">Domain</span>
-              </h1>
-            </motion.div>
+            {/* <motion.div
+            initial={{ opacity: 0, x: 500, y: 0 }} //initial position of text with opacity 0
+            animate={textControls} //text conntrol animation triggered.
+            transition={{ duration: 0.5 }}
+          > */}
+            <h1 className="sm:mb-[7px] ml-[11px] sm:ml-[15px] lg:ml-[30px] 2xl:ml-[53px] text-[12px] sm:text-[18px] font-semibold md:mb-[20px] md:text-3xl items-start text-[#FFFFFF] 2xl:text-[2.1vw] ">
+              Our <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#A03BCF] to-[#6F5BFF]">Domain</span>
+            </h1>
+            {/* </motion.div> */}
 
-            <div className="flex w-[80vw]  flex-wrap  gap-4 md:gap-5 2xl:gap-7 pl-[11px] sm:pl-[23px] md:pl-[77px] 2xl:pl-[47px]">
+            <div className="flex w-full 2xl:w-[100%] 3xl:w-[90%] flex-wrap gap-4 md:gap-5 2xl:gap-[30px] pl-8 md:pl-10 lg:pl-12 xl:w-[90%] 2xl:pl-[70px] pb-8 ">
               {OUR_DOMAINS.map((domain, index) => {
                 return (
                   <motion.div
+                          key={index}
+                          className="flex justify-center"
+                          initial={{ opacity: 0, y: 100 }} 
+                          animate={controls} 
+                          transition={{ duration: 1.1 + (index*0.1) }} 
+                          onAnimationComplete={handleSliderAnimationComplete}
+                        >
+                  <DomainCard
+                    isHome={true}
                     key={index}
-                    className="flex justify-center"
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={controls}
-                    transition={{ duration: 0.5 + (index * 0.1) }}
-                    onAnimationComplete={handleSliderAnimationComplete}
-                  >
-                    <DomainCard
-                      isHome={true}
-                      key={index}
-                      logo={<Image src={domain.imgSrc} alt="support" width={500} height={500}></Image>}
-                      heading={domain.name}
-                      about={domain.about}
-                    ></DomainCard>
+                    logo={<Image src={domain.imgSrc} alt="support" width={500} height={500}></Image>}
+                    heading={domain.name}
+                    about={domain.about}
+                  ></DomainCard>
                   </motion.div>
                 );
               })}
