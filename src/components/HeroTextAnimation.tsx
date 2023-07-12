a03e8309b6d3c7cf58cc9cedaf4621a2f58eb494
manishmh <manishmh982@gmail.com>
2023-07-12 18:22:31 +0530

    added text annimation to homepage hero section and minor changes

--- src/components/HeroTextAnimation.tsx
+++ src/components/HeroTextAnimation.tsx
@@ -5,27 +5,56 @@ type HeroTextAnimationProps = {
 };
 
 const HeroTextAnimation = ({ texts }: HeroTextAnimationProps) => {
-  const [currentText, setCurrentText] = useState(0);
-  console.log("current", currentText);
+  const [textAnimation, setTextAnimation] = useState(texts[0]);
+  const [translation, setTranslation] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentText < texts.length - 1) {
-        setCurrentText((prevText) => (prevText + 1) % texts.length);
-      }
-    }, 2000);
+    let currentIndex = 1;
+    let intervalId: ReturnType<typeof setInterval> | undefined;
 
-    return () => clearInterval(interval);
-  }, [currentText, texts.length]);
+    const animateText = () => {
+      const currentText = texts[currentIndex];
+      let newText = "";
+
+      for (let i = 0; i < currentText.length; i++) {
+        setTimeout(() => {
+          newText += currentText[i];
+          setTextAnimation(newText);
+
+          if (i === currentText.length - 1 && texts[currentIndex] != "Student" && currentIndex !== texts.length - 1) {
+            setTimeout(() => {
+              setTranslation((prevTranslation) => prevTranslation + 100);
+            }, currentText.length * 100);
+          }
+        }, i * 300);
+      }
+
+      currentIndex = (currentIndex + 1) % texts.length;
+
+      if (currentIndex === 0) {
+        clearInterval(intervalId);
+      }
+    };
+
+    intervalId = setInterval(animateText, texts.length * 450);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [texts]);
 
   return (
     <div className={`ml-2 flex w-[150px] overflow-hidden text-[#3B61CF]`}>
       {texts.map((text, index) => (
         <span
           key={index}
-          className={`w-full flex-shrink-0 duration-1000 ease-in-out transition-transform ${`-translate-x-[${currentText}00%]`}`}
+          className={`w-full flex-shrink-0 duration-1000 ease-in-out transition-transform`}
+          style={{
+            transform: `translateX(${-translation}%)`,
+            whiteSpace: "nowrap",
+          }}
         >
-          {text}
+          {textAnimation}
         </span>
       ))}
     </div>
--- src/components/gallery/GalleryFadeShow.tsx
+++ src/components/gallery/GalleryFadeShow.tsx
@@ -8,10 +8,10 @@ import { Devops_GALLERY, Eyespy_GALLERY, Frontend_GALLERY, ProjectWing_GALLERY }
 const GalleryFadeShow = () => {
   return (
     <div className="max-w-[1920px] grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 mx-auto pt-[80px] p-[5vw] ">
-      <GalleryFadeShowCard imgArray={ProjectWing_GALLERY} title="Project Wing" num={one} timer={5000} />
-      <GalleryFadeShowCard imgArray={Frontend_GALLERY} title="Frontend With Benefits" num={two} timer={10000} />
-      <GalleryFadeShowCard imgArray={Devops_GALLERY} title="DevOps" num={four} timer={8000} />
-      <GalleryFadeShowCard imgArray={Eyespy_GALLERY} title="EyeSpy Squid" num={three} timer={11000} />
+      <GalleryFadeShowCard imgArray={ProjectWing_GALLERY} title="Project Wing" num={1} timer={5000} />
+      <GalleryFadeShowCard imgArray={Frontend_GALLERY} title="Frontend With Benefits" num={2} timer={10000} />
+      <GalleryFadeShowCard imgArray={Devops_GALLERY} title="DevOps" num={3} timer={8000} />
+      <GalleryFadeShowCard imgArray={Eyespy_GALLERY} title="EyeSpy Squid" num={4} timer={11000} />
     </div>
   );
 };
