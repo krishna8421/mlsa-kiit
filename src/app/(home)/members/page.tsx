8dee95d08b769a157fd1d6a07d5f74884bf45f02
Harsh <harshku2222@gmail.com>
2023-07-12 15:22:15 +0530

    domain and members

--- src/app/(home)/members/page.tsx
+++ src/app/(home)/members/page.tsx
@@ -27,14 +27,14 @@ const Members = () => {
           <div className=" parallax-bg absolute top-0 bottom-0 left-0 right-0 bg-cover -z-10 min-h-full object-fill lg:object-cover"></div>
            <Image src="/Members/members.png" width={3000} height={100} alt="members" className="parallax-bg absolute top-[-50px] bottom-0 left-0 right-0 bg-cover -z-10 min-h-full object-cover"></Image>
            <div className=" absolute w-full bg-black left-0 h-[60px] bottom-0 font-semibold text-[32px]  z-30"> <span className="ml-[10px] md:ml-[35px] xl:ml-[80px] leading-[2rem] lg:leading-[3rem]  md:text-[40px] lg:text-[60px] 3xl:text-[80px] text-transparent bg-clip-text bg-gradient-to-r from-[#5051F9] to-[#AE86E0]">Our Team</span></div>
-          <Image src="/Rectangle-shadow.svg" width={400} height={400} alt="members" className=" hidden sm:block absolute right-0 bottom-[-450px] z-50"></Image>
+          <Image src="/Rectangle-shadow.svg" width={400} height={400} alt="members" className=" hidden sm:block absolute right-0 bottom-[-450px] z-30"></Image>
           {/* md:w-[400px] md:h-[400px] */}
         </div>
         <div className="parallax-section pt-[30px] md:pt-[100px] bg-black -mt-1 min-h-[100vh] p-[50px]  w-[100vw] relative overflow-hidden">
         <Image src="/Rectangle-shadow.svg" width={400} height={400} alt="members" className="hidden sm:block absolute right-0 top-[-370px] "></Image>
           
           <Webdev />
-          <Appdev />
+          {/* <Appdev />
           <Videoediting/>
           <Marketing />
           <MachineLearning />
@@ -42,7 +42,7 @@ const Members = () => {
           <Creative />
           <Arvr />
           <Cloud />
-          <Youtube />
+          <Youtube /> */}
 
 
         </div>
