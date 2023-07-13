2a954f606fa6d6d430a522fcce436e1e66c43fc6
manishmh <manishmh982@gmail.com>
2023-07-13 17:45:26 +0530

    navbar hover annimation

--- src/components/MobileView.tsx
+++ src/components/MobileView.tsx
@@ -13,7 +13,7 @@ const MobileView = ({ visible }: MobileMenuProps) => {
   }
   return (
     <>
-      <div className="bg-gradient-to-b from-[#042179] to-[#949fc0] fixed right-[24px] mt-2 flex w-[160px] flex-col gap-2 rounded-md pb-3 pt-2 text-center font-semibold md:hidden">
+      <div className="bg-gradient-to-b from-[#042179] to-[#949fc0] fixed right-[24px] mt-2 flex w-[190px] flex-col gap-2 rounded-md pb-3 pt-2 text-center font-semibold md:hidden">
         <NavbarItem href="/events" mobileView={true}>
           Events
         </NavbarItem>
