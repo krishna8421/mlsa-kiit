93740ee00be4884577bf94a77bf2a1e09f6cfbe0
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-07-18 14:18:55 +0530

    fix changes

--- src/app/(home)/members/page.tsx
+++ src/app/(home)/members/page.tsx
@@ -2,8 +2,8 @@
 
 import Appdev from "@/components/members/Appdev";
 import Arvr from "@/components/members/Arvr";
-import Creative from "@/components/members/Creative";
 import Cloud from "@/components/members/Cloud";
+import Creative from "@/components/members/Creative";
 import GraphicDesign from "@/components/members/Graphicdesign";
 import MachineLearning from "@/components/members/Machinelearning";
 import Marketing from "@/components/members/Marketing";
@@ -29,7 +29,7 @@ const Members = () => {
           <div className=" parallax-bg absolute top-0 bottom-0 left-0 right-0 bg-cover -z-10 min-h-full object-fill"></div>
           <Image src="/members.png" width={3000} height={100} alt="members" className="parallax-bg absolute top-[-50px] bottom-0 left-0 right-0 bg-cover -z-10 min-h-full object-cover"></Image>
           <div className=" absolute w-full bg-black left-0 h-[60px] bottom-0 font-semibold text-[32px]  z-30"> <span className="ml-[10px] md:ml-[35px] xl:ml-[80px] leading-[2rem] lg:leading-[3rem]  md:text-[40px] lg:text-[60px] 3xl:text-[80px] text-transparent bg-clip-text bg-gradient-to-r from-[#5051F9] to-[#AE86E0]">Our Team</span></div>
-          <Image src="/Rectangle-shadow.svg" width={400} height={400} alt="members" className=" hidden sm:block absolute right-0 bottom-[-563px] z-30"></Image>
+          <Image src="/Rectangle-shadow.svg" width={300} height={300} alt="members" className="absolute right-0 bottom-[-314px] z-30"></Image>
           {/* md:w-[400px] md:h-[400px] */}
         </div>
         <div className="parallax-section pt-[45px] md:pt-[100px] bg-black -mt-1 min-h-[100vh] lg:pl-[50px] md:pl-[20px] pl-[10px] lg:pr-[50px] md:pr-[30px] pr-[10px]  w-[100vw] relative overflow-hidden">
@@ -70,7 +70,7 @@ const Members = () => {
         </div>
       </div>
     </div>
-    
+
   </>
   )
 }
