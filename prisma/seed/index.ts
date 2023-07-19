b9daf40911f69b2c4cdfb3e9e9e55c9759032781
ADITYA NARAYANA CHOUDHURY <69430444+adityasubham03@users.noreply.github.com>
2023-07-19 15:10:53 +0530

    Added events and sponsors seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,14 +1,35 @@
-import { OUR_SPONSORS } from "./../../src/constants/index";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import blogsData from "../data/blogs.json";
+import sponsorsData from "../data/sponsors.json";
+import eventsData from "../data/events.json";
+
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
 
 const prisma = new PrismaClient();
 
 export async function seed() {
-  await prisma.blog.deleteMany();
+
+  await prisma.blog.deleteMany({});
   await prisma.blog.createMany({
     data: blogsData,
   });
+
+  await prisma.sponsor.deleteMany({});
+  await prisma.sponsor.createMany({
+    data: sponsorsData,
+  });
+
+  await prisma.event.deleteMany({});
+  await prisma.event.createMany({
+    data: eventsData,
+  });
+
+
 }
 
 seed();
