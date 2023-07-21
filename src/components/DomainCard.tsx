6dbb24fcbacf25161e4d0179952406a3f1ead937
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2023-07-21 20:56:07 +0530

    fixed

--- src/components/DomainCard.tsx
+++ src/components/DomainCard.tsx
@@ -46,7 +46,7 @@ const DomainCard = ({ isHome, logo, heading, about }: Props) => {
         </h1>
         <p
           className={`${isHome ? "md:ml-[32px]  4xl:text-xl" : "mx-5  pb-5 text-xl "
-            } ml-[5px] sm:ml-[12.92px] 2xl:mt-[46px] mb-1 lg:mt-[44px] md:mt-[35px] sm:mt-[25.83px] mt-[12.35px] 2xl:leading-[26.6px] md:leading-[15px] leading-normal font-light 2xl:text-[16px] md:text-[13px] text-[7px] text-[#c8c7c7]  transition duration-500 group-hover:text-[#FFFFFF]`}
+            } ml-[5px] sm:ml-[12.92px] 2xl:mt-[46px] mb-1 lg:mt-[44px] md:mt-[35px] sm:mt-[25.83px] mt-[12.35px] 2xl:leading-[26.6px] md:leading-[15px] leading-normal 2xl:text-[19px] md:text-[13px] text-[8px] text-[#7d8590] transition duration-500 group-hover:text-[#FFFFFF]`}
         >
           {about}
         </p>
